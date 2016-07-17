85303fde3d82769f234a14e9756bc7d42590a53f
aquila62 <aquila62@users.noreply.github.com>
2016-07-17 09:15:10 -0500

    Changed a comment.

--- ranwlk.c
+++ ranwlk.c
@@ -22,8 +22,8 @@ along with this program; if not, write to:
 
 /* this program performs a random walk in X Windows            */
 /* based on random input                                       */
-/* etausgen.c produces the input data through a pipe           */
-/* use the tstwlk.sh script to test this program with etausgen */
+/* ekissgen.c produces the input data through a pipe           */
+/* use the tstwlk.sh script to test this program with ekissgen */
 /* Press any key to quit                                       */
 
 /* to define the escape key */
